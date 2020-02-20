d8fbc02135a19d22d78a46e71a15227cebe12828
Ryan Hantak <rhantak@gmail.com>
2020-02-20 11:25:32 -0700

    Add functionality for users to post new words

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import $ from 'jquery'
-// const fetch = require("node-fetch");
 
 $(document).ready(() => {
-  let url = 'http://localhost:3000/api/v1/top_word'
-  var targetElement = document.getElementById('top-word')
+  function findTopWord() {
+    let url = 'http://localhost:3000/api/v1/top_word'
+    var targetElement = document.getElementById('top-word')
 
-  fetch(url)
-    .then(response => response.json())
-    .then(result => result["word"])
-    .then(word => targetElement.append(` ${Object.keys(word)}, ${Object.values(word)}`))
+    fetch(url)
+      .then(response => response.json())
+      .then(result => result["word"])
+      .then(word => targetElement.append(` ${Object.keys(word)}, ${Object.values(word)}`))
+  }
+  findTopWord()
+  var targetButton = document.getElementById('send-button')
 
+  targetButton.addEventListener('click', (e) => postWords(document.getElementById("user-words").value))
 
+  function postWords(words){
+    console.log(words)
+    var wordArray = words.split(" ")
+    wordArray.forEach(word => {
+      fetch('http://localhost:3000/api/v1/words', { method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+      },
+        body: JSON.stringify({"word": { "value": `${word}` }})
+      })
+    })
+  }
 })
